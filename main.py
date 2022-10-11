aee8df5a87d9f72d624adcba9f3f452777670625
amit <aamit2267@gmail.com>
2022-10-11 16:48:34 +0530

    f

---
+++ main.py
@@ -0,0 +1,30 @@
+import selenium
+import selenium.webdriver
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api():
+    driver = selenium.webdriver.Chrome("C:\\Users\\amita\\Downloads\\chromedriver_win32\\chromedriver.exe")
+    driver.get('https://messmenu.epizy.com/')
+    d={}
+    d['date'] = driver.find_elements(by=selenium.webdriver.common.by.By.TAG_NAME, value='h3')[0].text
+    d['time'] = driver.find_elements(by=selenium.webdriver.common.by.By.TAG_NAME, value='h2')[0].text
+    d['occ'] = driver.find_elements(by=selenium.webdriver.common.by.By.TAG_NAME, value='h3')[1].text.encode("ascii", "ignore").decode()
+    li = driver.find_elements(by=selenium.webdriver.common.by.By.TAG_NAME, value='li')
+    d['li'] = [i.text for i in li]
+    if len(d['li'])==0:
+        td = driver.find_elements(by=selenium.webdriver.common.by.By.TAG_NAME, value='td')
+        d['td'] = {}
+        for i in range(0,len(td)):
+            if i % 2 == 0:
+                d['td'][td[i].text] = td[i+1].text.encode("ascii", "ignore").decode().replace(" ", "")
+    driver.close()
+    with open('data.json', 'w') as outfile:
+        json.dump(d, outfile, indent=4)
+    return jsonify(d)
+
+if __name__ == '__main__':
+    app.run()
